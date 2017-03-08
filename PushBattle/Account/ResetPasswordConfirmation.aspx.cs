069dce252a205bc71556b536ba8d3ffa0e51c60c
av <av@ALEXVALENCIE9CB>
2017-03-07 17:50:43 -0800

    Add project files.

---
+++ PushBattle/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace PushBattle.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ PushBattle/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PushBattle.Startup))]
+namespace PushBattle
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
